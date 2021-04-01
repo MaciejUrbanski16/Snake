98155f1107dcfab4739127e4a85da3bc3945df66
Maciej U <urbanskimac@gmail.com>
2021-04-01 14:52:29 +0200

    Initial commit

---
+++ Google_tests/test1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Admin on 01.04.2021.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(pierwa,dwa){
+    EXPECT_EQ(1,1);
+}
